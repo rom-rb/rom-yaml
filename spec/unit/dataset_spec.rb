5dd67dc3b03ed32e121b2b8022be2b73e7a15968
Peter Suschlik <ps@neopoly.de>
2015-02-17 10:50:04 +0100

    Add specs for ROM::YAML::Dataset

---
+++ spec/unit/dataset_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+require 'rom/lint/spec'
+
+describe ROM::YAML::Dataset do
+  let(:data) { [{ id: 1 }, { id: 2 }] }
+  let(:dataset) { ROM::YAML::Dataset.new(data) }
+
+  it_behaves_like "a rom enumerable dataset"
+
+  it "symbolizes keys" do
+    dataset = ROM::YAML::Dataset.new(["foo" => 23])
+    expect(dataset.to_a).to eq([{ foo: 23 }])
+  end
+
+  it "does not symbolizes keys recursively" do
+    dataset = ROM::YAML::Dataset.new(["foo" => { "bar" => :baz }])
+    expect(dataset.to_a).to eq([foo: { "bar" => :baz }])
+  end
+end
